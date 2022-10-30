ec69de287e2cc5894115dd5fed0c45946fde13d5
Ilya <blacklord@list.ru>
2022-10-30 20:32:33 +1000

    upload edit to json/base64

--- back/resources/upload.py
+++ back/resources/upload.py
@@ -1,21 +1,33 @@
-from flask import  make_response, jsonify, request
+from flask import make_response, jsonify, request
 from flask_restful import Resource as ResFree
+import base64
 
 class Upload(ResFree):
     def post(self):
-        file = request.file['file']
-        if file and self.verifyExt(file.filename):
-            try:
+        jsonData = request.get_json()
 
-                return make_response(jsonify({"message": "Successful"}), 202)
-            except FileNotFoundError as e:
-                return make_response(jsonify({"error": "true", "message": "File read error"}), 401)
+        try:
+            date = jsonData['date']
+            name = jsonData['name']
+            result = jsonData['result']
+            size = jsonData['size']
+
+            files = base64.decode(result)
+
+            for file in files:
+                pass
+
+            return make_response(jsonify({"message": "Successful"}), 202)
+        except KeyError:
+            return make_response(jsonify({"error": "true", "message": "File read error"}), 401)
+    '''    
         else:
             return make_response(jsonify({"error": "true", "message": "File not found or wrong extension"}), 401)
-
-    def verifyExt(self, filename):
+    '''
+    '''def verifyExt(self, filename):
         ext = filename.rsplit('.', 1)[1]
         if ext == "xls" or ext == "XLS" or ext == "xlsx" or ext == "XLSX":
             return True
-
+    
         return False
+    '''
