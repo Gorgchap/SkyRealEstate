9460aea827e8aa48e93c143812e1cc993a288f69
Ilya <blacklord@list.ru>
2022-11-05 20:12:29 +1000

    add api update

--- back/resources/services.py
+++ back/resources/services.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from flask import make_response, jsonify, request, send_file
 from flask_restful import Resource as ResFree
-from model import rs_files, rs_buildings, rs_flats, user
+#from model import rs_files, rs_buildings, rs_flats, user
 from api import db_session
 import pandas as pd
 
